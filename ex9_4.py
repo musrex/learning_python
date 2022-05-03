7880de79f74d11a640637324b2f92bf9ae61268e
musrex <leandro.n.lopez@gmail.com>
2022-05-02 22:00:12 -0400

    modified:   ex9_4.py

--- ex9_4.py
+++ ex9_4.py
@@ -1,7 +1,27 @@
-name = input("Enter file:")
+name = input("Enter file: ")
 if len(name) < 1:
     name = "mbox-short.txt"
 handle = open(name)
-  
-for lines in handle:
+address = list()
+emails = dict()
+for line in handle:
+    if line.startswith("From "):
+        line = line.split()
+        address = line[1]
+
+for addess in line:
+    if address not in emails:
+                emails[address] = emails.get(address, 0) + 1
+
+maxe = 0
+for key in emails:
+    if emails[key] > maxe:
+        maxe = key
+
+print(emails, emails[maxe])
+
+        
+        #emails[address] = emails.get(address, 0) + 1
+        
+#print(emails)
 
